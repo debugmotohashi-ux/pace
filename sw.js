4285f4c44832b50a4c31f07ec87ef85f8cb8351c
debugmotohashi-ux <debug.motohashi@gmail.com>
2026-06-30 15:32:56 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,13 @@
+const CACHE='pace-v1';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./icon-180.png','./icon-maskable-512.png'];
+self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS).catch(()=>{})));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const req=e.request; if(req.method!=='GET')return;
+  const isNav=req.mode==='navigate'||(req.headers.get('accept')||'').includes('text/html');
+  if(isNav){
+    e.respondWith(fetch(req).then(r=>{const cp=r.clone();caches.open(CACHE).then(c=>c.put('./index.html',cp));return r;}).catch(()=>caches.match('./index.html').then(r=>r||caches.match('./'))));
+  }else{
+    e.respondWith(caches.match(req).then(r=>r||fetch(req).then(res=>{const cp=res.clone();caches.open(CACHE).then(c=>c.put(req,cp));return res;}).catch(()=>r)));
+  }
+});
